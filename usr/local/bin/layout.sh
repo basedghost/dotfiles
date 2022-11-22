eb1c24ee85f437b246f3472a15dff39b14af2117
basedghost <111021033+basedghost@users.noreply.github.com>
2022-11-22 02:56:54 +0000

    updated xrandr layout.

---
+++ usr/local/bin/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 3600x0 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-4 --primary --mode 1920x1080 --rate 144 --pos 1680x0 --rotate normal --output DP-5 --off
